a1ada78b52c628309bb1b54ce6e7fb325a57c600
ManuelCyrus <manuelcyrus.mda@gmail.com>
2025-12-23 22:23:27 +0000

    creating string functions

---
+++ src/utils/str/core_utils.c
@@ -0,0 +1,62 @@
+
+/*! 
+*@brief core_lenght return the all length of one string 
+*/  
+int core_length(char *str){
+
+    int i = 0;
+
+    if(!str){
+        return 0;
+    }
+
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    
+    return i;
+}
+
+
+/*! 
+*@brief core_str_join join 2 strings 
+*/   
+char *core_str_join(char *s1, char *s2){
+
+    if(!s1 || !s2){
+        return "s";
+    }
+    else if(s1 && !s2){
+        return s1;
+    }
+    else if(!s1 && s2){
+        return s2;
+    }
+
+    int strlen = core_length(s1)+core_length(s2);
+    char *new = malloc((strlen+1)*sizeof(char));
+    int i = 0;
+    int j =0;
+    while (s1[i] != '\0')
+    {
+        new[i] = s1[i];
+        i++;
+    }
+
+    while (s2[j] != '\0')
+    {
+        new[i] = s2[j];
+        i++;
+        j++;
+    }
+
+    new[i] = '\0';
+    
+    return new;
+
+    
+}
+
+
+
