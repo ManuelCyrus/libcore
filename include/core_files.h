f884d927dc99836df7db1a69a6b0d6cb0725e4e1
ManuelCyrus <manuelcyrus.mda@gmail.com>
2026-03-08 22:33:27 +0000

    get next line

--- include/core_files.h
+++ include/core_files.h
@@ -1,6 +1,15 @@
 #ifndef CORE_FILES
 #define CORE_FILES
 
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 42
+#endif
+
+
+#if BUFFER_SIZE < 0
+#undef BUFFER_SIZE
+#define BUFFER_SIZE 42
+#endif
 
 
 char *get_next_line(int fd);
--- src/files/get_next_line.c
+++ src/files/get_next_line.c
@@ -1,6 +1,39 @@
-#include "../../include/core_files.h"
+#include "../../include/core.h"
+
 
 
 char *get_next_line(int fd){
+
+
+
+
+
     
 }
+
+
+static ssize_t read_file(int fd , char *buffer){
+       char *mem;
+       ssize_t bytes_read=0;
+
+    if(fd < 0 || !buffer)
+        return (-1);
+
+        while (1)
+        {
+            bytes_read = read(fd,buffer,BUFFER_SIZE);
+            
+            if(bytes_read > 0)
+                buffer[bytes_read] = '\0';
+            mem = core_str_join(mem,buffer);
+            
+        
+
+        }
+        
+    return (bytes_read);
+
+}
+
+
+
